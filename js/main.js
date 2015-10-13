e81a964e6ca8ebc51ea32eb32c25ee2edc5e5d35
rensramos <rensramos17@gmail.com>
2015-10-13 19:03:40 +0800

    Update index page

---
+++ js/main.js
@@ -0,0 +1,24 @@
+var fs = fs || {};
+fs.fsMainFunction = function() {
+    //fs.scrollRevealItems();
+    //fs.stickyNavbar();
+    //fs.iconAnimation();
+};
+
+fs.scrollRevealItems = function(){
+    window.sr = new scrollReveal();
+}
+
+fs.iconAnimation = function() {
+    $('#icon-transition').on('click', function () {
+        $(this).toggleClass('open');
+    });
+}
+
+fs.stickyNavbar = function(){
+    $(".category-nav-container").sticky({topSpacing:100});
+}
+
+$(function(){
+    fs.fsMainFunction();
+});
