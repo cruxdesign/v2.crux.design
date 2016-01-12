c48a6508dbb95ac24af5c0c35e38dad2ec80c838
rensramos <rensramos17@gmail.com>
2016-01-12 18:00:57 +0800

    Add responsiveness in work section

--- js/home.js
+++ js/home.js
@@ -1,26 +1,55 @@
+//$(document).ready( function() {
+//    $(function() {
+//        $("#grid").mason({
+//            itemSelector: '.block',
+//            ratio: 1,
+//            sizes: [
+//                [1,1]
+//            ],
+//            promoted: [
+//                ['p1', 2, 1],
+//                ['p2', 3, 1],
+//                ['p3', 3, 2],
+//                ['p4', 2, 2],
+//                ['p5', 2, 1],
+//                ['p6', 3, 1]
+//            ],
+//            columns: [
+//                [0, 480, 6]
+//            ],
+//            filler: {
+//                itemSelector: '.filler',
+//                filler_class: 'mason-filler',
+//                keepDataAndEvents: false
+//            },
+//            gutter: 5,
+//            layout: 'fluid'
+//        })
+//    });
+//});
+
 $(document).ready( function() {
     $(function() {
         $("#grid").mason({
             itemSelector: '.block',
+            columns: [
+                [320, 768, 1],
+                [991, 1280, 5]
+            ],
             ratio: 1,
             promoted: [
-                ['p1', 2, 1.5],
-                ['p2', 3, 1.5],
+                ['p1', 2, 1],
+                ['p2', 3, 1],
                 ['p3', 3, 2],
                 ['p4', 2, 2],
-                ['p5', 2, 1.5],
-                ['p6', 3, 1.5]
+                ['p5', 2, 1],
+                ['p6', 3, 1]
             ],
-            //columns: [
-            //    [320, 767, 1],
-            //    [768, 1440, 6],
-            //    [1440, 1920, 6]
-            //],
+            layout: 'fluid',
             filler: {
                 keepDataAndEvents: false
             },
-            layout: 'fluid',
-            gutter: 8
-        });
+            gutter: 5
+        })
     });
 });
