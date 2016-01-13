082886c010e0760da6e2a89f9b4ea2757fa9938c
rensramos <rensramos17@gmail.com>
2016-01-13 11:35:33 +0800

    Add transition effect on work title

--- js/home.js
+++ js/home.js
@@ -1,55 +1,43 @@
-//$(document).ready( function() {
-//    $(function() {
-//        $("#grid").mason({
-//            itemSelector: '.block',
-//            ratio: 1,
-//            sizes: [
-//                [1,1]
-//            ],
-//            promoted: [
-//                ['p1', 2, 1],
-//                ['p2', 3, 1],
-//                ['p3', 3, 2],
-//                ['p4', 2, 2],
-//                ['p5', 2, 1],
-//                ['p6', 3, 1]
-//            ],
-//            columns: [
-//                [0, 480, 6]
-//            ],
-//            filler: {
-//                itemSelector: '.filler',
-//                filler_class: 'mason-filler',
-//                keepDataAndEvents: false
-//            },
-//            gutter: 5,
-//            layout: 'fluid'
-//        })
-//    });
-//});
+var crux = crux || {};
 
-$(document).ready( function() {
-    $(function() {
-        $("#grid").mason({
-            itemSelector: '.block',
-            columns: [
-                [320, 768, 1],
-                [991, 1280, 5]
-            ],
-            ratio: 1,
-            promoted: [
-                ['p1', 2, 1],
-                ['p2', 3, 1],
-                ['p3', 3, 2],
-                ['p4', 2, 2],
-                ['p5', 2, 1],
-                ['p6', 3, 1]
-            ],
-            layout: 'fluid',
-            filler: {
-                keepDataAndEvents: false
-            },
-            gutter: 5
-        })
+crux.init = function(){
+    crux.workGrid();
+    crux.workHover();
+};
+
+crux.workGrid = function() {
+    $("#grid").mason({
+        itemSelector: '.block',
+        columns: [
+            [320, 768, 1],
+            [991, 1280, 5]
+        ],
+        ratio: 1,
+        promoted: [
+            ['p1', 2, 1],
+            ['p2', 3, 1],
+            ['p3', 3, 2],
+            ['p4', 2, 2],
+            ['p5', 2, 1],
+            ['p6', 3, 1]
+        ],
+        layout: 'fluid',
+        filler: {
+            keepDataAndEvents: false
+        },
+        gutter: 5
     });
+};
+
+crux.workHover = function(){
+    var workItem = $("#work-section #grid .block .caption")
+    workItem.hover(function(){
+        $(this).toggleClass('active');
+    });
+
+};
+
+$(function(){
+    crux.init();
 });
+
