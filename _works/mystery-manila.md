d0a900e6674528e4b7422b408d4ec32f543d7043
rensramos <rensramos17@gmail.com>
2016-01-27 18:44:46 +0800

    add owl carousel

--- _works/mystery-manila.md
+++ _works/mystery-manila.md
@@ -29,9 +29,15 @@ work-info:
       
 project-type-icon: '/img/project/website/web_icon.png'
 
-project-description:
-    - description: 
+project-detail:
+    - description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    - description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
     
+project-process:
+    - title: Wireframe
+    - title: Color Palette
+    - title: Typography
+    - title: Responsive Design
     
-css:    
+   
 ---
--- js/main.js
+++ js/main.js
@@ -5,19 +5,26 @@ crux.homeFunction = function() {
     crux.shrinkNavbarMenu();
 };
 
+
+
 crux.shrinkNavbarMenu = function(){
     var brand = $('.navbar-brand');
     var logo = brand.find('.nav-logo');
-
     $(window).scroll(function() {
-        if ($(document).scrollTop() > 50 && $(document).width() >= 992) {
-            $('nav').addClass('navbar-shrink');
-            logo.attr('src', '/img/logo/crux-logo-inverse.png');
-        } else {
-            $('nav').removeClass('navbar-shrink');
-            logo.attr('src', '/img/logo/crux-brand.png');
+        if(window.innerWidth >= 992) {
+            if ($(document).scrollTop() > 50) {
+                $('nav').addClass('navbar-shrink');
+                logo.attr('src', '/img/logo/crux-logo-inverse.png');
+            } else {
+                $('nav').removeClass('navbar-shrink');
+                logo.attr('src', '/img/logo/crux-brand.png');
+            }
         }
     });
+    if (window.innerWidth <= 991) {
+        $('nav').addClass('navbar-shrink');
+        logo.attr('src', '/img/logo/crux-logo-inverse.png');
+    }
 };
 
 crux.showMenuOnClick = function(){
