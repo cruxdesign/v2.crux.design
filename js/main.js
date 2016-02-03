aa9cd2c1ed5169caaebcc0ea9f4065113e8eb792
rensramos <rensramos17@gmail.com>
2016-02-03 14:59:08 +0800

    update navbar

--- js/main.js
+++ js/main.js
@@ -1,30 +1,25 @@
 var crux = crux || {};
 crux.homeFunction = function() {
     crux.showMenuOnClick();
-    crux.iconAnimation();
-    crux.changeIcon
-
-
+    //crux.iconAnimation();
 };
-
-var showMenuOnClick = $('nav.navbar-default .container-fluid');
-
 
 
 crux.showMenuOnClick = function(){
     var showMenuOnClick = $('nav.navbar-default .container-fluid');
-    var iconChangeOnClick = $('#icon-transition');
     $(function() {
         $('.toggle-nav').click(function() {
             showMenuOnClick.toggleClass('show-nav');
             return false;
         });
     });
+
     //close burger menu
     $(document).keyup(function(e) {
         if (e.keyCode == 27) {
             showMenuOnClick.removeClass('show-nav');
-            iconChangeOnClick.removeClass('open');
+            //iconChangeOnClick.removeClass('open');
+
         }
     });
 };
