39581a6275cae10978b4a089554f70961a13e6a7
rensramos <rensramos17@gmail.com>
2016-02-04 13:44:01 +0800

    add formspree

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 var crux = crux || {};
 crux.homeFunction = function() {
     crux.showMenuOnClick();
-    //crux.iconAnimation();
+    crux.showProjectPlannerOnClick();
 };
 
+crux.showProjectPlannerOnClick = function(){
+    var showProjectPlanner = $('nav.navbar-default .container-fluid');
+    $(function(){
+       $('.toggle-planner').click(function() {
+           showProjectPlanner.toggleClass('show-project-planner');
+           showProjectPlanner.removeClass('show-nav');
+           return false;
+       });
+    });
+};
 
 crux.showMenuOnClick = function(){
     var showMenuOnClick = $('nav.navbar-default .container-fluid');
     $(function() {
         $('.toggle-nav').click(function() {
             showMenuOnClick.toggleClass('show-nav');
+            showMenuOnClick.removeClass('show-project-planner');
             return false;
         });
     });
@@ -18,6 +29,8 @@ crux.showMenuOnClick = function(){
     $(document).keyup(function(e) {
         if (e.keyCode == 27) {
             showMenuOnClick.removeClass('show-nav');
+            showMenuOnClick.removeClass('show-project-planner');
+
             //iconChangeOnClick.removeClass('open');
 
         }
